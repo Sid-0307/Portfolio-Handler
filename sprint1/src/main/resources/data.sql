8ab44e5aa0371e1a5a30301b80ac3ab50a33a7e3
Sid-0307 <siddharthshankar03@gmail.com>
2024-09-13 23:09:15 +0530

    Updated data.sql

--- sprint1/src/main/resources/data.sql
+++ sprint1/src/main/resources/data.sql
@@ -1,8 +1,6 @@
-INSERT INTO stock (symbol, name, invested_amt, quantity , revenue) VALUES
+INSERT IGNORE INTO stock (symbol, name, invested_amt, quantity , revenue) VALUES
   ('AAPL', 'Apple Inc.', 100, 1, 100)
-WHERE NOT EXISTS (SELECT * FROM stock WHERE symbol = 'AAPL');
 
-INSERT INTO transaction_history (transaction_id,price, quantity, stock_symbol, time_stamp, total, trade)
+INSERT IGNORE INTO transaction_history (transaction_id,price, quantity, stock_symbol, time_stamp, total, trade)
 VALUES
   (1,100, 1, 'AAPL', '22-10-2023 12:34:56', 100.00, 'BUY')
-WHERE NOT EXISTS (SELECT * FROM transaction_history WHERE transaction_id = 1);
