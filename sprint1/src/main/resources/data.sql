64a6dc4661e48716a3549b8c17d3b9bc12842792
Sid-0307 <siddharthshankar03@gmail.com>
2024-09-13 23:16:26 +0530

    updated data.sql

--- sprint1/src/main/resources/data.sql
+++ sprint1/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO stock (symbol, name, invested_amt, quantity , revenue) VALUES
-  ('AAPL', 'Apple Inc.', 100, 1, 100)
+  ('AAPL', 'Apple Inc.', 100, 1, 100);
 
 INSERT IGNORE INTO transaction_history (transaction_id,price, quantity, stock_symbol, time_stamp, total, trade)
 VALUES
-  (1,100, 1, 'AAPL', '22-10-2023 12:34:56', 100.00, 'BUY')
+  (1,100, 1, 'AAPL', '22-10-2023 12:34:56', 100.00, 'BUY');
